d101a25a886de20da65b2ff10146c8f28eb00d4e
Casey Leonard <casey@amphibian.com>
2012-09-15 13:38:09 -0400

    update to sorta work

--- src/main/java/edu/psu/sweng/ff/controller/MemberController.java
+++ src/main/java/edu/psu/sweng/ff/controller/MemberController.java
@@ -1,16 +1,141 @@
 package edu.psu.sweng.ff.controller;
 
+import java.net.URI;
+import java.util.UUID;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import edu.psu.sweng.ff.common.Member;
+import edu.psu.sweng.ff.dao.MemberDAO;
 
 @Path("/member")
 public class MemberController {
 
-	@GET
-	public Response getMember() {
+	private final static String TOKEN_HEADER = "X-UserToken";
 
-		return Response.ok().entity("member").build();
+	@Context UriInfo uriInfo;
+	
+	@POST
+	@Path("/authenticate")
+	@Consumes("application/x-www-form-urlencoded")
+	public Response authenticate(
+		@FormParam("username") String userName,
+		@FormParam("password") String password
+		)
+	{
+		
+		MemberDAO dao = new MemberDAO();
+		String token = dao.authenticateUser(userName, password);
+		
+		Response response = null;
+		if (token != null) {
+			response = Response.ok().header(TOKEN_HEADER, token).build();
+			System.out.println("authenticated " + userName);
+		} else {
+			response = Response.status(Status.UNAUTHORIZED).build();
+		}
+		
+		return response;
+		
+	}
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response getTokenOwner(
+		@HeaderParam(TOKEN_HEADER) String token
+	    )
+	{
+
+		Member requester = this.lookupByToken(token);
+		if (requester == null) {
+			return Response.status(Status.UNAUTHORIZED).build();
+		}
+		System.out.println(requester.getUserName() + " is loading self");
+
+		return Response.ok().entity(requester).build();
+		
+	}
+	
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Path("/{userName}")
+	public Response getMember(
+		@HeaderParam(TOKEN_HEADER) String token,
+		@PathParam("userName") String userName
+	    )
+	{
+
+		Member requester = this.lookupByToken(token);
+		if (requester == null) {
+			return Response.status(Status.UNAUTHORIZED).build();
+		}
+		System.out.println(requester.getUserName() + " is loading " + userName);
+		
+		MemberDAO dao = new MemberDAO();
+		Member m = dao.loadByUserName(userName);
+		return Response.ok().entity(m).build();
+		
+	}
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Path("/id/{id}")
+	public Response getMemberById(
+		@HeaderParam(TOKEN_HEADER) String token,
+		@PathParam("id") int id
+	    )
+	{
+
+		Member requester = this.lookupByToken(token);
+		if (requester == null) {
+			return Response.status(Status.UNAUTHORIZED).build();
+		}
+		System.out.println(requester.getUserName() + " is loading user " + id);
+		
+		MemberDAO dao = new MemberDAO();
+		Member m = dao.loadById(id);
+		return Response.ok().entity(m).build();
+		
+	}
+
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response createMember(
+		Member member
+		)
+	{
+		
+		MemberDAO dao = new MemberDAO();
+		member.setAccessToken(UUID.randomUUID().toString());
+		member.setPassword("password"); //TODO: use a real password
+		member.setId(dao.nextMemberId());
+		dao.store(member);
+		
+		UriBuilder ub = uriInfo.getAbsolutePathBuilder();
+		URI memberUri = ub.path("id/" + member.getId()).build();
+		
+		return Response.created(memberUri).build();
+		
+	}
+	
+	private Member lookupByToken(String t) {
+		
+		MemberDAO dao = new MemberDAO();
+		return dao.loadByToken(t);
 		
 	}
 	
---
+++ src/main/java/edu/psu/sweng/ff/dao/MemberDAO.java
@@ -0,0 +1,74 @@
+package edu.psu.sweng.ff.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.psu.sweng.ff.common.Member;
+
+public class MemberDAO {
+
+	private static Map<Integer, Member> idToMemberMap = new HashMap<Integer, Member>();
+
+	private static Map<String, Member> userNameToMemberMap = new HashMap<String, Member>();
+
+	private static Map<String, Member> tokenToMemberMap = new HashMap<String, Member>();
+
+	private static int nextId = 1;
+	
+	static {
+		
+		Member tester = new Member();
+		tester.setEmail("test@test.com");
+		tester.setFirstName("Test");
+		tester.setLastName("Tester");
+		tester.setHideEmail(false);
+		tester.setId(0);
+		tester.setMobileNumber("555-555-5555");
+		tester.setAccessToken("1111-2222-3333");
+		tester.setPassword("password");
+		tester.setUserName("test");
+		
+		idToMemberMap.put(tester.getId(), tester);
+		userNameToMemberMap.put(tester.getUserName(), tester);
+		tokenToMemberMap.put(tester.getAccessToken(), tester);
+		
+	}
+	
+	public String authenticateUser(String u, String p) {
+		
+		Member m = loadByUserName(u);
+		if (m == null) {
+			return null;
+		} else {
+			if (m.getPassword().equals(p)) {
+				return m.getAccessToken();
+			} else {
+				return null;
+			}
+		}
+		
+	}
+	
+	public Member loadById(int id) {
+		return idToMemberMap.get(id);
+	}
+	
+	public Member loadByUserName(String un) {
+		return userNameToMemberMap.get(un);
+	}
+	
+	public Member loadByToken(String t) {
+		return tokenToMemberMap.get(t);
+	}
+	
+	public void store(Member m) {
+		idToMemberMap.put(m.getId(), m);
+		userNameToMemberMap.put(m.getUserName(), m);
+		tokenToMemberMap.put(m.getAccessToken(), m);
+	}
+	
+	public int nextMemberId() {
+		return nextId++;
+	}
+	
+}
